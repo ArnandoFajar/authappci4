3532ca45ddfc2cbd8b6bff6a7fc376968038ca19
arnandofajar <arnandofajar@gmail.com>
2023-05-11 15:25:47 +0700

    first init

---
+++ app/Database/Migrations/2023-05-11-082036_Users.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => '11',
+            ],
+            'email'          => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ],
+            'password'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '255',
+            ],
+            'name'       => [
+                'type'           => 'VARCHAR',
+                'constraint'     => '100',
+            ],
+            'created_at' => [
+                'type'           => 'DATETIME',
+                'null'            => true,
+            ],
+            'updated_at' => [
+                'type'           => 'DATETIME',
+                'null'            => true,
+            ]
+
+        ]);
+        $this->forge->addPrimaryKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
